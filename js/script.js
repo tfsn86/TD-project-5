5664229ba2f4598eab257c71e86f384753dfdbb6
tfsn <tfsn86@gmail.com>
2019-07-30 11:49:23 +0200

    Update script.js
    Created function for hiding and displaying employees when searching. Added code comments.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const employees = 'https://randomuser.me/api/?results=12';
+const employeesURL = 'https://randomuser.me/api/?results=12';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.querySelector('#gallery');
-const body = document.querySelector('body');
+const headerTxtContainer = document.querySelector('.header-text-container h1');
+
+// Variables for the "No employees found!" message.
+let noEmployeesFoundMessage;
+let noEmployeesFoundMessageDisplayed = false;
 
 // Creates the markup for the searchbar and appends it to the search container.
 const searchBar = document.createElement('form');
@@ -9,11 +13,67 @@ searchBar.innerHTML = `
   <form action="#" method="get">
   <input type="search" id="search-input" class="search-input" placeholder="Search...">
   <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
-  </form
+  </form>
 `;
 
 searchContainer.appendChild(searchBar);
 
+/**
+ * Hides or displays employees.
+ * @param searchInput - Input typed by the user in the searchbar.
+ */
+function hideOrDisplayEmployees(searchInput) {
+	const employeeList = gallery.children;
+
+	let hiddenEmployees = 0;
+	let foundEmployees = 0;
+	let errorDiv = document.createElement('P');
+	errorDiv.id = 'no-employee-found';
+
+	for (let i = 0; i < employeeList.length; i++) {
+		const item = employeeList[i];
+		const name = item.querySelector('#name');
+		if (name.textContent.includes(searchInput.toLowerCase())) {
+			item.style.display = '';
+			foundEmployees += 1;
+		} else {
+			item.style.display = 'none';
+			hiddenEmployees += 1;
+		}
+	}
+
+	if (hiddenEmployees === 12 && foundEmployees === 0) {
+		if (!noEmployeesFoundMessageDisplayed) {
+			let html = `No employees found!`;
+			errorDiv.innerHTML = html;
+			gallery.insertAdjacentElement('afterend', errorDiv);
+			noEmployeesFoundMessageDisplayed = true;
+		}
+	} else if (foundEmployees >= 1) {
+		if (noEmployeesFoundMessageDisplayed) {
+			noEmployeesFoundMessage = document.getElementById('no-employee-found');
+			document.body.removeChild(noEmployeesFoundMessage);
+			noEmployeesFoundMessageDisplayed = false;
+		}
+	}
+}
+
+/**
+ * Converts the employee birthday dateinput to a format which is easier to read and understand.
+ * @param date
+ */
+function reformatBirthday(date) {
+	let month = date.substring(5, 7);
+	let day = date.substring(8, 10);
+	let year = date.substring(2, 4);
+	let newDate = month + '/' + day + '/' + year;
+	return newDate; // Example output: 14/09/86
+}
+
+/**
+ * Handles the API request and converts the string to JSON.
+ * @param url - Takes an API resource as a parameter.
+ */
 async function getJSON(url) {
 	try {
 		const response = await fetch(url);
@@ -23,15 +83,10 @@ async function getJSON(url) {
 	}
 }
 
-function reformat_birthdate(date) {
-	let month = date.substring(5, 7);
-	let day = date.substring(8, 10);
-	let year = date.substring(2, 4);
-	// Example output: 14/09/86
-	let new_date = month + '/' + day + '/' + year;
-	return new_date;
-}
-
+/**
+ * Constructs variables containing employee data.
+ * @param url - Takes JSON data as a parameter.
+ */
 async function getEmployees(url) {
 	const peopleJSON = await getJSON(url);
 
@@ -50,8 +105,11 @@ async function getEmployees(url) {
 	return Promise.all(profiles);
 }
 
-// Gallery HTML
-function cardHTML(data) {
+/**
+ * Creates the gallery HTML containing all employees with data for each employee.
+ * @param data - Takes employee data as a parameter
+ */
+function galleryHTML(data) {
 	data.map(person => {
 		const cardDiv = document.createElement('div');
 		gallery.appendChild(cardDiv);
@@ -67,29 +125,36 @@ function cardHTML(data) {
     </div>
     `;
 
+		// Listens for clicks on an employee card and opens a modal with further information about the employee.
 		cardDiv.addEventListener('click', () => {
 			const modalDiv = document.createElement('modal');
-			body.insertBefore(modalDiv, body.childNodes[6]);
+			gallery.insertAdjacentElement('afterend', modalDiv);
 			modalDiv.className = 'modal-container';
 			modalDiv.innerHTML = `
       <div class="modal">
-      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-      <div class="modal-info-container">
-        <img class="modal-img" src="${person.large}" alt="profile picture">
-        <h3 id="name" class="modal-name cap">${person.name}</h3>
-        <p class="modal-text">${person.email}</p>
-        <p class="modal-text cap">${person.location}</p>
-        <hr>
-        <p class="modal-text">Phone: ${person.phoneNumber}</p>
-        <p class="modal-text">Address: ${person.address}</p>
-        <p class="modal-text">Birthday: ${reformat_birthdate(
-					person.birthday
-				)}</p>
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+          <img class="modal-img" src="${person.large}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${person.name}</h3>
+          <p class="modal-text">${person.email}</p>
+          <p class="modal-text cap">${person.location}</p>
+          <hr>
+          <p class="modal-text">Phone: ${person.phoneNumber}</p>
+          <p class="modal-text">Address: ${person.address}</p>
+          <p class="modal-text">Birthday: ${reformatBirthday(
+						person.birthday
+					)}</p>
+        </div>
+        <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
       </div>
-      </div>`;
+      `;
 
 			const modalCloseBtn = document.querySelector('.modal-close-btn');
 
+			// Listens for click on the close button on the modal window.
 			modalCloseBtn.addEventListener('click', () => {
 				document.querySelector('.modal-container').style.display = 'none';
 			});
@@ -97,4 +162,25 @@ function cardHTML(data) {
 	});
 }
 
-getEmployees(employees).then(cardHTML);
+/**
+ * Executes the program.
+ */
+getEmployees(employeesURL)
+	.then(galleryHTML)
+	.catch(e => {
+		headerTxtContainer.textContent = 'Something went wrong!';
+		console.error(e);
+	});
+
+// Listens for a search submit and displays the relevant employees.
+document.getElementById('serach-submit').addEventListener('click', function(e) {
+	e.preventDefault();
+	let userInput = document.getElementById('search-input').value;
+	hideOrDisplayEmployees(userInput);
+});
+
+// Listens for letters typed in the search bar and displays the relevant employees.
+document.getElementById('search-input').addEventListener('keyup', function() {
+	let userInput = document.getElementById('search-input').value;
+	hideOrDisplayEmployees(userInput);
+});
